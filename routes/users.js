2b0146e34bed20420f2d53aba4f95cbb2e8ee97d
Your Name <you@example.com>
2024-09-22 16:00:15 -0400

    {}

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const userRouter = require("express").Router(); // creating a router
 const { getCurrentUser, updateProfile } = require("../controllers/users");
-const  auth  = require("../middlewares/auth");
+const { auth } = require("../middlewares/auth");
 
 userRouter.get("/me", auth, getCurrentUser);
 userRouter.patch("/me", auth, updateProfile);
