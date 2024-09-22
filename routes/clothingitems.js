73450b1a72049cec4aa7ac4c876fb4c821207d10
Your Name <you@example.com>
2024-09-22 00:14:11 -0400

    errors

--- routes/clothingitems.js
+++ routes/clothingitems.js
@@ -1,16 +1,20 @@
-const itemRouter = require("express").Router(); // creating a router
+const router = require("express").Router();
+const auth = require("../middlewares/auth");
 const {
-  getItems,
   createItem,
+  getItems,
   deleteItem,
-  dislikeItem,
   likeItem,
-} = require("../controllers/clothingitems");
-const { auth } = require("../middlewares/auth");
+  dislikeItem,
+} = require("../controllers/clothingItems");
 
-itemRouter.get("/", getItems);
-itemRouter.post("/", auth, createItem);
-itemRouter.delete("/:itemId", auth, deleteItem);
-itemRouter.put("/:itemId/likes", auth, likeItem);
-itemRouter.delete("/:itemId/likes", auth, dislikeItem);
-module.exports = itemRouter;
+router.get("/", getItems);
+
+router.use(auth);
+
+router.post("/", createItem);
+router.delete("/:itemId", deleteItem);
+router.put("/:itemId/likes", likeItem);
+router.delete("/:itemId/likes", dislikeItem);
+
+module.exports = router;
